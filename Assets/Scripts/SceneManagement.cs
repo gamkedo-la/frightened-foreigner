f740e9c6c44df7af13a24642f87034d229280965
stebssbets <stebssbets@yahoo.com>
2019-07-18 20:47:52 +0200

    added DontDestroyOnLoad(transform.gameObject) to the SceneManager object to keep the reference to the background music alive during Title Screen to Intro CutScene to Cemetery Level navigation

--- Assets/Scripts/SceneManagement.cs
+++ Assets/Scripts/SceneManagement.cs
@@ -6,36 +6,45 @@ using UnityEngine.SceneManagement;
 public class SceneManagement : MonoBehaviour
 {
 
-    static FMOD.Studio.EventInstance TitleScreenMusic;
-    static FMOD.Studio.PLAYBACK_STATE TitleScreenMusicPlaybackState;
+    static public FMOD.Studio.EventInstance TitleScreenMusic;
+    static public FMOD.Studio.PLAYBACK_STATE TitleScreenMusicPlaybackState;
+
+    public GameObject SceneManagerFromHierarchy;
 
     private void Awake()
     {
+
         TitleScreenMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/titleScreenMusicV1");
-        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        TitleScreenMusic.start();
+        //TitleScreenMusic.getPlaybackState(out TitleScreenMusicPlaybackState);
+        //Debug.Log(TitleScreenMusicPlaybackState);
+        //if (TitleScreenMusicPlaybackState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        //{
+            TitleScreenMusic.start();
+        //}   
     }
 
     // Update is called once per frame
     void Update()
     {
-        TitleScreenMusic.getPlaybackState(out TitleScreenMusicPlaybackState);
-        Debug.Log(TitleScreenMusicPlaybackState);
+        //TitleScreenMusic.getPlaybackState(out TitleScreenMusicPlaybackState);
+       // Debug.Log(TitleScreenMusicPlaybackState);
     }
 
     public void LoadCemeteryLevelFromIntroCutscene()
     {
         SceneManager.LoadScene("Cemetery Level");
         TitleScreenMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        DontDestroyOnLoad(transform.gameObject);
     }
 
     public void LoadIntroCutScene()
     {
         SceneManager.LoadScene("Intro CutScene");
+        DontDestroyOnLoad(transform.gameObject);
     }
 }
