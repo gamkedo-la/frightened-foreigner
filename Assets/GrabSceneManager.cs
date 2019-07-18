f1f872475f651f65fc031fb4381395c3b754c9f9
stebssbets <stebssbets@yahoo.com>
2019-07-18 21:11:05 +0200

    added GrabSceneManager.cs to the Skip Intro button in the Intro CutScene, the Skip Intro Button functions now

---
+++ Assets/GrabSceneManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrabSceneManager : MonoBehaviour
+{
+
+    private GameObject preservedSceneManager;
+
+    public Button SkipIntroButton;
+
+    private SceneManagement SceneManagementScript;
+
+    private void Awake()
+    {
+        preservedSceneManager = GameObject.Find("SceneManager");
+        Debug.Log(preservedSceneManager);
+        SceneManagementScript = preservedSceneManager.GetComponent<SceneManagement>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SkipIntroButton.onClick.AddListener(SceneManagementScript.LoadCemeteryLevelFromIntroCutscene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
