7dc8a05ffce535ee271cb8b46891ee695a5b911f
stebssbets <stebssbets@yahoo.com>
2019-07-20 16:59:50 +0200

    WIP: Adding audio settings to main menu and cutscene to help audio levels during meetings, just trying to commit before potential conflicts

--- Assets/Scripts/ToggleAudioSettingsMenu.cs
+++ Assets/Scripts/ToggleAudioSettingsMenu.cs
@@ -1,21 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ToggleAudioSettingsMenu : MonoBehaviour
 {
     public Transform audioSettingsCanvas;
     public Transform pauseMenuCanvas;
+    public Transform TitleScreenCanvas;
+
+    
 
     private void Awake()
     {
-        //audioSettingsCanvas = GameObject.Find("AudioSettingsCanvas");
-        //pauseMenuCanvas = GameObject.Find("PauseCanvas");
+        
     }
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -26,22 +29,49 @@ public class ToggleAudioSettingsMenu : MonoBehaviour
 
     public void toggleAudioSettingsMenu()
     {
-        if (audioSettingsCanvas.gameObject.activeInHierarchy == false)
+        
+        if (audioSettingsCanvas.gameObject.activeInHierarchy == false)//if the audio settings menu is inactive
         {
-            audioSettingsCanvas.gameObject.SetActive(true);
-            pauseMenuCanvas.gameObject.SetActive(false);
+            audioSettingsCanvas.gameObject.SetActive(true);//make the audio settings menu active, then check.....
 
+            if ("Main Menu" == SceneManager.GetActiveScene().name)//if the current scene is the main menu
+            {
+                TitleScreenCanvas.gameObject.SetActive(false);//hide the Title screen
+            }
+            else if ("Intro CutScene" == SceneManager.GetActiveScene().name)//if the current scene is the intro cutscene
+            {
+                //we don't need to do anything, just making it clear in comprehensive logic
+            }
+            else if ("Cemetery Level" == SceneManager.GetActiveScene().name) //if the current scene is the cemetery
+            {
+                if (pauseMenuCanvas.gameObject.activeInHierarchy == true)//and we're accessing the audio settings from the pause menu
+                {
+                    pauseMenuCanvas.gameObject.SetActive(false);//hide the pause menu
+                }
+            }
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.None;
         }
-        else
+
+        else if ((audioSettingsCanvas.gameObject.activeInHierarchy == true))//when leaving the audio settings menu
         {
-            audioSettingsCanvas.gameObject.SetActive(false);
-            pauseMenuCanvas.gameObject.SetActive(true);
-            //Time.timeScale = 0;
-            //Cursor.lockState = CursorLockMode.Confined;
-            //Cursor.lockState = CursorLockMode.Locked;
-            //Time.timeScale = 1;
+            audioSettingsCanvas.gameObject.SetActive(false);//hide the audio settings menu
+
+            if ("Main Menu" == SceneManager.GetActiveScene().name)//if the current scene is the main menu
+            {
+                TitleScreenCanvas.gameObject.SetActive(true);//show the title screen
+            }
+            else if ("Intro CutScene" == SceneManager.GetActiveScene().name)//if the current scene is the intro cutscene
+            {
+                //we don't need to do anything, just making it clear in comprehensive logic
+            }
+            else if ("Cemetery Level" == SceneManager.GetActiveScene().name) //if the current scene is the cemetery
+            {
+                if (pauseMenuCanvas.gameObject.activeInHierarchy == false)//and we're leaving the audio settings, which navigates back to the pause menu
+                {
+                    pauseMenuCanvas.gameObject.SetActive(false);//show the pause menu
+                }
+            }
         }
     }
 }
