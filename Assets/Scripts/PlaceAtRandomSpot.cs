556e357c3d699c73d9b5b0d3ab5866b0931ba9ba
KornelGM <KORNik_k@wp.pl>
2019-07-26 10:20:44 +0200

    Script for placing things at random spots, can specify the range of places to put it

---
+++ Assets/Scripts/PlaceAtRandomSpot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceAtRandomSpot : MonoBehaviour
+{
+	[SerializeField] private Vector3 originPoint = Vector3.zero;
+	[SerializeField] private Vector3 placeBoxSize = Vector3.one;
+
+	void Start( )
+	{
+		Place( );
+	}
+
+	void OnDrawGizmosSelected( )
+	{
+		Gizmos.DrawWireCube( originPoint, placeBoxSize );
+	}
+
+	[ContextMenu("Place at random position")]
+	private void Place( )
+	{
+		transform.position = GetRandomPosition( );
+	}
+
+	private Vector3 GetRandomPosition( )
+	{
+		Vector3 randomPos = new Vector3
+		(
+			Random.Range( -placeBoxSize.x / 2, placeBoxSize.x / 2 ),
+			Random.Range( -placeBoxSize.y / 2, placeBoxSize.y / 2 ),
+			Random.Range( -placeBoxSize.z / 2, placeBoxSize.z / 2 )
+		);
+
+		return randomPos + originPoint;
+	}
+}
