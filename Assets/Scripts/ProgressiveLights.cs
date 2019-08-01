de5d74d3743fe5782aeea9aca7bca25218e3cf56
Barış Köklü <bariskoklu@gmail.com>
2019-08-01 20:35:46 +0300

    Ambient gets creepier (less lights) as player progress.

---
+++ Assets/Scripts/ProgressiveLights.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressiveLights : MonoBehaviour
+{
+    private List<GameObject> allLightObjects = new List<GameObject>();
+
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            allLightObjects.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public void MakeAmbientCreepier()
+    {
+        RenderSettings.ambientIntensity -= 0.4f;
+        for (int i = 0; i < allLightObjects.Count; i++)
+        {
+            allLightObjects[i].GetComponent<Light>().intensity -= 0.3f;
+        }
+    }
+}
