a00d2030f7b9ee4ee213a2b9d9e3b8005b91316e
KornelGM <KORNik_k@wp.pl>
2019-08-13 14:51:55 +0200

    Added auto-magically closing door (entrance)

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	private void OnTriggerExit( Collider other )
+	{
+		if ( !other.CompareTag( "Player" ) )
+			return;
+
+		GetComponent<Animator>( ).enabled = true;
+		GetComponent<Collider>( ).enabled = false;
+	}
+}
