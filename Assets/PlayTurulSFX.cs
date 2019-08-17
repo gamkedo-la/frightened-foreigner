e61101175a5e391dc316491eeb60ed7ff97b8b22
stebssbets <stebssbets@yahoo.com>
2019-08-17 21:05:36 +0200

    turul flies to the tree of life while flapping at the start of the game, he also squawks, temporarily removed the 3D attenuation to make it audible

---
+++ Assets/PlayTurulSFX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTurulSFX : MonoBehaviour
+{
+
+    private FMOD.Studio.EventInstance TurulSquawkSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TurulSquawkSound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/TurulSquawk");
+        //TurulSquawkSound.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayTurulSquawkSound()
+    {
+        TurulSquawkSound.start();
+    }
+}
---
+++ Assets/TurulMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurulMovement : MonoBehaviour
+{
+    public GameObject TurlWingsOpen;
+    public GameObject TurulWingsClosed;
+
+    public float flapsPerSecond = 4f;
+    private int flapCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
