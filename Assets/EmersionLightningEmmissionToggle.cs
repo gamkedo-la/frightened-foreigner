0ab093aabd8f1e1c5c9927b83986a93e2c60c8d3
stebssbets <stebssbets@yahoo.com>
2019-08-27 11:37:06 +0200

    lightning, sparks, and emersion flash, test trigger with L key

---
+++ Assets/EmersionLightningEmmissionToggle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmersionLightningEmmissionToggle : MonoBehaviour
+{
+
+    private ParticleSystem EmersionLightning;
+    public ParticleSystem Sparks;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EmersionLightning = gameObject.GetComponent<ParticleSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var EmersionLightningEmitter = EmersionLightning.emission;
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            EmersionLightningEmitter.enabled = true;
+            EmersionLightning.Emit(1);
+            Sparks.Emit(1);
+        }
+    }
+}
