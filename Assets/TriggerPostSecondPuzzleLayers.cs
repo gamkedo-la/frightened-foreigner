04126254e36c748b16752b620e5ae5de13eb4a97
Jaime Rivas <jaimerivas81@outlook.com>
2019-09-12 20:15:18 -0700

    All second puzzle music triggers functional (minus the vocals since those will be a bit trickier); piano and metals layers slightly increased in volume

---
+++ Assets/TriggerPostSecondPuzzleLayers.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerPostSecondPuzzleLayers : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance PostBathroomMusic;
+    private FMOD.Studio.PLAYBACK_STATE PostBathroomMusicPlayback;
+    private bool isPostBathroomMusicPlaying;
+
+    void Awake()
+    {
+        BoxCollider box = gameObject.GetComponent<BoxCollider>();
+        box.isTrigger = true;
+    }
+
+    private void Start()
+    {
+        PostBathroomMusic = SceneManagement.PostBathroomMusic;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PostBathroomMusic.getPlaybackState(out PostBathroomMusicPlayback);
+        isPostBathroomMusicPlaying = PostBathroomMusicPlayback != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+
+        if(isPostBathroomMusicPlaying)
+        {
+            switch (gameObject.name)
+            {
+                case "postSecondPuzzlePercLayerTrigger":
+                    PostBathroomMusic.setParameterValue("PercIn", 1f);
+                    break;
+                case "postSecondPuzzleFluteLayerTrigger":
+                    PostBathroomMusic.setParameterValue("FluteIn", 1f);
+                    break;
+                case "postSecondPuzzlePianoLayerTrigger":
+                    PostBathroomMusic.setParameterValue("PianoIn", 1f);
+                    break;
+                case "postSecondPuzzleGhostBellsLayerTrigger":
+                    PostBathroomMusic.setParameterValue("GhostBellsIn", 1f);
+                    break;
+                case "postSecondPuzzleMetalsLayerTrigger":
+                    PostBathroomMusic.setParameterValue("MetalsIn", 1f);
+                    break;
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        PostBathroomMusic.getPlaybackState(out PostBathroomMusicPlayback);
+        isPostBathroomMusicPlaying = PostBathroomMusicPlayback != FMOD.Studio.PLAYBACK_STATE.STOPPED;
+
+        if (isPostBathroomMusicPlaying)
+        {
+            switch (gameObject.name)
+            {
+                case "postSecondPuzzlePercLayerTrigger":
+                    PostBathroomMusic.setParameterValue("PercIn", 0f);
+                    break;
+                case "postSecondPuzzleFluteLayerTrigger":
+                    PostBathroomMusic.setParameterValue("FluteIn", 0f);
+                    break;
+                case "postSecondPuzzlePianoLayerTrigger":
+                    PostBathroomMusic.setParameterValue("PianoIn", 0f);
+                    break;
+                case "postSecondPuzzleGhostBellsLayerTrigger":
+                    PostBathroomMusic.setParameterValue("GhostBellsIn", 0f);
+                    break;
+                case "postSecondPuzzleMetalsLayerTrigger":
+                    PostBathroomMusic.setParameterValue("MetalsIn", 0f);
+                    break;
+            }
+        }
+    }
+}
