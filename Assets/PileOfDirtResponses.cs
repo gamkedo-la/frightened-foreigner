c00f8269f6eadb1d0c1fb2615e23bda87e3efa5a
stebssbets <stebssbets@yahoo.com>
2019-09-14 19:51:13 +0200

    created a player's grave area, player now has to get the dirt from the player's grave with the shovel rather than the ground as kind of a workaround to the raycasts of lockview getting blocked by FMOD trigger zones and to add more character to the scene, graves added for vanessa and charlie for extra scene props

---
+++ Assets/PileOfDirtResponses.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PileOfDirtResponses : MonoBehaviour
+{
+
+    public FMOD.Studio.EventInstance groundDoesntCare;
+    public FMOD.Studio.EventInstance IHaveDirt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundDoesntCare = FMODUnity.RuntimeManager.CreateInstance("event:/ItemInteractions/GroundDoesntCare");
+        IHaveDirt = FMODUnity.RuntimeManager.CreateInstance("event:/ItemInteractions/IHaveDirt");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CorrectInteraction()
+    {
+        IHaveDirt.start();
+    }
+
+    public void IncorrectResponse()
+    {
+        groundDoesntCare.start();
+    }
+}
