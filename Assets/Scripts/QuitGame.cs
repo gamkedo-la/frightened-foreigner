ff20c12870991de626aff5a3ff2231be6c795af1
Cyriel De Neve <cyriel@shaw.ca>
2019-09-17 20:31:01 -0600

    Added function to Exit Button on Main Menu to quit the application.  This won't work on WebGL builds

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        // This won't work in WebGL
+        // Debug.Log("Attempting to quit");
+        Application.Quit();
+    }
+}
