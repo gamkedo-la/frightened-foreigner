bce7d7856981a70f5eaee51fa106300fd393f326
KornelGM <KORNik_k@wp.pl>
2019-09-23 12:26:38 +0200

    Mausoleum animation should now play correctly

--- Assets/AudioEventTrigger.cs
+++ Assets/AudioEventTrigger.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class AudioEventTrigger : MonoBehaviour
 {
-    
-    
+
+
     public GameObject secondPuzzleLayerTriggerObject;
     private TriggerPostSecondPuzzleLayers secondPuzzleLayerTriggerScript;
 
@@ -14,13 +14,13 @@ public class AudioEventTrigger : MonoBehaviour
     FMOD.Studio.EVENT_CALLBACK callback;
 
     public GameObject mausoleum;
-    private Animation mausoleumShakeAnimation;
+    private Animator mausoleumShakeAnimation;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
         secondPuzzleLayerTriggerScript = secondPuzzleLayerTriggerObject.GetComponent<TriggerPostSecondPuzzleLayers>();
         postBathroomMusic = secondPuzzleLayerTriggerScript.PostBathroomMusic;
         //postBathroomMusic = SceneManagement.PostBathroomMusic;
@@ -29,14 +29,14 @@ public class AudioEventTrigger : MonoBehaviour
         postBathroomMusic.setCallback(callback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
         //audioTrack.EventInstance.setCallback(callback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
 
-        mausoleumShakeAnimation = mausoleum.GetComponent<Animation>();
+        mausoleumShakeAnimation = mausoleum.GetComponent<Animator>();
 
     }
 
-    static FMOD.RESULT CallBackTriggered(FMOD.Studio.EVENT_CALLBACK_TYPE type, FMOD.Studio.EventInstance instance, IntPtr parameterPtr)
+	private FMOD.RESULT CallBackTriggered(FMOD.Studio.EVENT_CALLBACK_TYPE type, FMOD.Studio.EventInstance instance, IntPtr parameterPtr)
     {
         Debug.Log("Callback triggered");
-        //mausoleumShakeAnimation.Play();
+        mausoleumShakeAnimation.Play( 0 );
         var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
         Debug.Log("Parameter name: " + parameter.name);
         return FMOD.RESULT.OK;
