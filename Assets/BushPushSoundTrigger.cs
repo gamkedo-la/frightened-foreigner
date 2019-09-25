dcf114e159eec53422ca26e8aa56017969a32e93
stebssbets <stebssbets@yahoo.com>
2019-09-25 20:02:11 +0200

    made a 'BushPush' multisound, added trigger zones

---
+++ Assets/BushPushSoundTrigger.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BushPushSoundTrigger : MonoBehaviour
+{
+
+    private FMOD.Studio.EventInstance BushPushSound;
+    //private bool playerWasWalkingLastFrame = false;
+    private bool PlayerIsWalking = false;
+    private FMOD.Studio.PLAYBACK_STATE BushPushSoundPlaybackState;
+
+    private void Awake()
+    {
+        BoxCollider box = gameObject.GetComponent<BoxCollider>();
+        box.isTrigger = true;
+
+        BushPushSound = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/BushPush");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        {
+            //Debug.Log("Player is Walking");
+            PlayerIsWalking = true;
+        }
+        if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("s") || Input.GetKeyUp("d"))
+        {
+            PlayerIsWalking = false;
+        }
+
+        BushPushSound.getPlaybackState(out BushPushSoundPlaybackState);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player entered bush zone");
+        BushPushSound.start();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!PlayerIsWalking)
+        {
+            BushPushSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+        else if (PlayerIsWalking && BushPushSoundPlaybackState != FMOD.Studio.PLAYBACK_STATE.PLAYING && BushPushSoundPlaybackState != FMOD.Studio.PLAYBACK_STATE.STARTING)
+        {
+            BushPushSound.start();
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        BushPushSound.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
