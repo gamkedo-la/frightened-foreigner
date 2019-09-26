711d4de8331bbe7ef2b46167d5b6ab455a8dd9d0
stebssbets <stebssbets@yahoo.com>
2019-09-26 19:09:29 +0200

    convergence 2 plotted and triggerable

--- Assets/triggerConvergenceAnimation.cs
+++ Assets/triggerConvergenceAnimation.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class triggerConvergenceAnimation : MonoBehaviour
 {
+    Animator cloudConvergenceAnimator;
+    int convergence2Hash = Animator.StringToHash("cloudConvergence2");
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cloudConvergenceAnimator = GetComponent<Animator>();
+        cloudConvergenceAnimator.SetBool("convergence2Bool", false);
     }
 
     // Update is called once per frame
@@ -17,5 +21,14 @@ public class triggerConvergenceAnimation : MonoBehaviour
         {
             gameObject.GetComponent<Animator>().enabled = true;
         }
+        if (Input.GetKeyDown("v"))
+        {
+            triggerSecondCloudConvergence();
+        }
+    }
+
+    public void triggerSecondCloudConvergence()
+    {
+        cloudConvergenceAnimator.Play("Storm Convergence 2");
     }
 }
