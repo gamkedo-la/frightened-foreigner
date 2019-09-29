4b5ad0dcbd4a041c4c0c95b550325e9ab5d6c1ce
stebssbets <stebssbets@yahoo.com>
2019-09-29 20:11:19 +0200

    cleaned up some debug logs

--- Assets/BushPushSoundTrigger.cs
+++ Assets/BushPushSoundTrigger.cs
@@ -42,7 +42,7 @@ public class BushPushSoundTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Player entered bush zone");
+        
         BushPushSound.start();
     }
 
--- Assets/Scripts/ProgressiveLights.cs
+++ Assets/Scripts/ProgressiveLights.cs
@@ -32,32 +32,28 @@ public class ProgressiveLights : MonoBehaviour
         if (Input.GetKeyDown("b"))
         {
             lightsShouldBeDimming = true;
-            Debug.Log("recognizing b press");
-            Debug.Log("target dim amount " + targetDimAmount);
-            Debug.Log("current temp dim amount " + currentTempDimAmount);
+            
         }
         if (lightsShouldBeDimming && currentTempDimAmount > targetDimAmount)
         {
             MakeAmbientCreepier();
-            Debug.Log("lights should be dimming?" + lightsShouldBeDimming);
+            
         }
         if (currentTempDimAmount <= targetDimAmount)
         {
             
             lightsShouldBeDimming = false;
             currentTempDimAmount = 0.0f;
-            Debug.Log("target dim amount: " + targetDimAmount);
+            
         }
 
         if (fogShouldBeGettingFoggier)
         {
-            Debug.Log("Should be getting foggier");
-            Debug.Log("Bathroom Puzzle? " + PuzzleManagement.PlayerIsDoingBathroomPuzzle);
-            Debug.Log("Cat Puzzle?" + PuzzleManagement.PlayerIsDoingCatPuzzle);
+            
             if (PuzzleManagement.PlayerIsDoingBathroomPuzzle && RenderSettings.fogEndDistance > afterBathroomLightningFogAmount)
             {
                 RenderSettings.fogEndDistance -= fogGradient;
-                Debug.Log("fog end distance: " + RenderSettings.fogEndDistance);
+                
             }
             else if (PuzzleManagement.PlayerIsDoingBathroomPuzzle && RenderSettings.fogEndDistance <= afterBathroomLightningFogAmount)
             {
@@ -67,7 +63,7 @@ public class ProgressiveLights : MonoBehaviour
             if (PuzzleManagement.PlayerIsDoingCatPuzzle && RenderSettings.fogEndDistance > bathroomCutsceneFogAmount)
             {
                 RenderSettings.fogEndDistance -= bathroomCutsceneFogGradient;
-                Debug.Log("fogEndDistance: " + RenderSettings.fogEndDistance);
+                
             }
             else if (PuzzleManagement.PlayerIsDoingCatPuzzle && RenderSettings.fogEndDistance <= bathroomCutsceneFogAmount)
             {
@@ -96,6 +92,6 @@ public class ProgressiveLights : MonoBehaviour
     {
         RenderSettings.fog = true;
         fogShouldBeGettingFoggier = true;
-        Debug.Log("turn on fog triggered");
+        
     }
 }
