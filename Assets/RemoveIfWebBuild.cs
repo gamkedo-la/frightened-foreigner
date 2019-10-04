14246f397ac0cecc41632456d054e4bbc3405d39
Gamkedo <chris@gamkedo.com>
2019-10-04 10:56:17 -0700

    Credits integrated. Minor menu refactor to use vertical layout group for buttons, code added to remove Exit button when in webGL (since application.quit only works in native builds)

---
+++ Assets/RemoveIfWebBuild.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveIfWebBuild : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            Debug.Log("Non-native build, removing exit button");
+            Destroy(gameObject);
+        }
+    }
+}
