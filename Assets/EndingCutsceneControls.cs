f37fed2d1ef9580b0fd1187493cf1947cfc3a30f
stebssbets <stebssbets@yahoo.com>
2019-10-12 22:00:00 +0700

    WIP: Ending cutscene

---
+++ Assets/EndingCutsceneControls.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCutsceneControls : MonoBehaviour
+{
+
+    public GameObject player;
+    Animator playerAnimator;
+    public GameObject Turul;
+    Animator TurulAnimator;
+    public GameObject sword;
+    Animator swordAnimator;
+    public GameObject Dragon;
+    Animator DragonAnimator;
+    public GameObject key;
+    Animator keyAnimator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DragonAnimator = Dragon.GetComponent<Animator>();
+        TurulAnimator = Turul.GetComponent<Animator>();
+        swordAnimator = sword.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            playEndingCutscene();
+        }
+    }
+
+    public void playEndingCutscene()
+    {
+        //DragonAnimator.Play("DragonEndingCutscene");
+        //DragonAnimator.enabled = true;
+        TurulAnimator.Play("TurulEndingCutscene");
+        //sword.SetActive(true);
+        //swordAnimator.enabled = true;
+    }
+
+    public void activateSword()
+    {
+        sword.SetActive(true);
+    }
+}
---
+++ Assets/dropSwordForEndingCutscene.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropSwordForEndingCutscene : MonoBehaviour
+{
+
+    public GameObject sword;
+    Animator swordAnimator;
+
+    public GameObject dragon;
+    Animator dragonAnimator;
+
+    public GameObject key;
+    Animator keyAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        swordAnimator = sword.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void activateAndAnimateSword()
+    {
+        sword.SetActive(true);
+        //swordAnimator.enabled = true;
+    }
+
+    public void activateAndAnimateDragon()
+    {
+        dragon.SetActive(true);
+        //dragonAnimator.enabled = true;
+    }
+
+    public void pickUpSwordAndThrowAnimation()
+    {
+        swordAnimator.Play("pickUpSwordAndThrowAtDragon");
+        
+    }
+
+    public void keyDropAnimation()
+    {
+        dragon.SetActive(false);
+        sword.SetActive(false);
+        key.SetActive(true);
+    }
+}
