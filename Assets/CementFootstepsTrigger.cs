0adeb52f577254ddba87f680270318be2d6fd936
stebssbets <stebssbets@yahoo.com>
2019-10-18 14:32:21 +0700

    added audio occlusion (volume ducks and eq filter) to rain and thunder sounds when player enters the bathroom after being trapped in the cemetery. leaving the bathroom turns off the occlusion

--- Assets/CementFootstepsTrigger.cs
+++ Assets/CementFootstepsTrigger.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CementFootstepsTrigger : MonoBehaviour
 {
 
+    public GameObject generalRainSoundHolder;
+    public GameObject thunderSoundHolder;
+
     void Awake()
     {
         BoxCollider box = gameObject.GetComponent<BoxCollider>();
@@ -14,12 +17,25 @@ public class CementFootstepsTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         PlayFootsteps.Footsteps.setParameterValue("SwitchSteps", 2.0f);
+        if (!PuzzleManagement.PlayerIsDoingBathroomPuzzle && !PuzzleManagement.PlayerIsDoingMoneyPuzzle)
+        {
+            generalRainSoundHolder.GetComponent<GeneralRainSoundsScript>().generalRainSounds.setParameterValue("rainTypes", 1.0f);
+            generalRainSoundHolder.GetComponent<GeneralRainSoundsScript>().generalRainSounds.setParameterValue("bathroomRainFilter", 1.0f);
+            thunderSoundHolder.GetComponent<StormSoundControls>().StormSoundInstance.setParameterValue("StormIntensity", 2.0f);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         PlayFootsteps.Footsteps.setParameterValue("SwitchSteps", 0f);
+        if (!PuzzleManagement.PlayerIsDoingBathroomPuzzle && !PuzzleManagement.PlayerIsDoingMoneyPuzzle)
+        {
+            generalRainSoundHolder.GetComponent<GeneralRainSoundsScript>().generalRainSounds.setParameterValue("rainTypes", 2.0f);
+            generalRainSoundHolder.GetComponent<GeneralRainSoundsScript>().generalRainSounds.setParameterValue("bathroomRainFilter", 0.0f);
+            thunderSoundHolder.GetComponent<StormSoundControls>().StormSoundInstance.setParameterValue("StormIntensity", 1.0f);
 
+
+        }
     }
 
     // Start is called before the first frame update
